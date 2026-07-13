44c05d619b9bdc4b6cf964da47acbf6754b7ba31
Alice Musindo <musindo.alice@gmail.com>
2026-07-13 10:51:44 +0200

    Update 03_Data_Cleaning.sql

--- sql/03_Data_Cleaning.sql
+++ sql/03_Data_Cleaning.sql
@@ -1,3 +1,20 @@
+/*
+=========================================================
+Bright TV Audience Analytics
+File: 01_Data_Exploration.sql
+Author: Alice Musindo
+Date: July 2026
+
+Description:
+This script explores the structure and quality of the Bright TV datasets before transformation and analysis.
+
+Tools:
+- Databricks SQL
+- SQL
+
+=========================================================
+*/
+
 CASE
 WHEN Province=' ' THEN 'Uncategorized'
 WHEN Province='None' THEN 'Uncategorized'
