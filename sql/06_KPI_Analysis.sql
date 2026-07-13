246e0d159ecd046d38fa268b89f0c14c297e4662
Alice Musindo <musindo.alice@gmail.com>
2026-07-13 10:52:48 +0200

    Update 06_KPI_Analysis.sql

--- sql/06_KPI_Analysis.sql
+++ sql/06_KPI_Analysis.sql
@@ -1,3 +1,21 @@
+
+/*
+=========================================================
+Bright TV Audience Analytics
+File: 01_Data_Exploration.sql
+Author: Alice Musindo
+Date: July 2026
+
+Description:
+This script explores the structure and quality of the Bright TV datasets before transformation and analysis.
+
+Tools:
+- Databricks SQL
+- SQL
+
+=========================================================
+*/
+
 -- Total Subscribers
 SELECT COUNT(DISTINCT UserID)
 FROM workspace.default.bright_tv_userprofiles;
